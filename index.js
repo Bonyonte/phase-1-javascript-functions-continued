a122b55fcb6cb731eea0d70535e9424c5c0e3c0b
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-10 11:55:12 +0300

    commit tree 6c5b34ab0aecbd02feaa1672451df16390b891d0

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(target){
+    if (typeof target === 'string'){
+    return `This Saturday, I want to ${target}!`;
+    }
+    else{
+    return "This Saturday, I want to roller-skate!";
+    }
+}
+saturdayFun('exercise');
+function mondayWork(study){
+    if(typeof study === 'string'){
+    return `This Monday, I will ${study}.`;
+    }
+    else{
+        return 'This Monday, I will go to the office.' ;
+    }
+}
+mondayWork();
+
+function wrapAdjective(argument){
+    const adjective = function(argument2){
+        if(argument === '*'){
+        return `You are ${argument}${argument2}${argument}!`
+    }
+    else if(argument === '||'){
+            return `You are ${argument}${argument2}${argument}!`
+    }
+}
+
+    return adjective;
+}
+wrapAdjective("*")("a dedicated programmer"); 
